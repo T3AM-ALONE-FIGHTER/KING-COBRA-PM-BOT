31ddcb40a8ed3465ff4fa1fb909d9cd0c7fa6c0a
ディペッシュ <83916289+Itzdipesh@users.noreply.github.com>
2021-07-11 15:41:37 +0530

    Update repo.py

--- DipeshBot/plugins/repo.py
+++ DipeshBot/plugins/repo.py
@@ -7,6 +7,6 @@ DIPESH_USER = [1809900087]
     events.NewMessage(pattern="^/repo ?(.*)"))
 async def _(event):
   if event.sender_id == DIPESH_USER:
-    await Dipesh.send_message(HEY MASTER HERE IS YOUR REPO\n\nhttps://github.com/itzdipesh/DIPESH-PM-BOT)
+    await Dipesh.send_message(HEY MASTER HERE IS YOUR REPO\n\n https://github.com/itzdipesh/DIPESH-PM-BOT)
   else:
     await event.reply("**BHAI YAAR THUM GAAND MARAO**")
