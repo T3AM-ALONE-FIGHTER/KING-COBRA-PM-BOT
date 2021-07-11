4aea0e8d7b91dadcad94ee1340e31caf3f5f2d5a
ディペッシュ <83916289+Itzdipesh@users.noreply.github.com>
2021-07-11 15:17:22 +0530

    Update repo.py

--- DipeshBot/plugins/repo.py
+++ DipeshBot/plugins/repo.py
@@ -1,12 +1,12 @@
-from .. import BotzHub
+from .. import Dipesh
 from telethon import events, Button
 
-SMEX_USER = [1809900087]
+DIPESH_USER = [1809900087]
 
-@BotzHub.on(
+@Dipesh.on(
     events.NewMessage(pattern="^/repo ?(.*)"))
 async def _(event):
-  if event.sender_id == SMEX_USER:
-    await BotzHub.send_message(HEY MASTER HERE IS YOUR REPO\n\nhttps://github.com/R2K-VENOM/TelethonBot)
+  if event.sender_id == DIPESH_USER:
+    await Dipesh.send_message(HEY MASTER HERE IS YOUR REPO\n\nhttps://github.com/itzdipesh/DIPESH-PM-BOT)
   else:
     await event.reply("**BHAI YAAR THUM GAAND MARAO**")
